7dceea021c1d2ebdff85cb3fb413a3ff76bc328b
Jerrod Styrk <jstyrk@citadel.edu>
2020-10-23 12:01:13 -0700

    Insert a reload page function to reset the answers and page display

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function totalValue(num1, num2, num3, num4, num5) {
   return num1 + num2 + num3 + num4 + num5;
 }
 
+function reloadPage() {
+  window.location.reload();
+}
+
 // User interface Logic
 $(document).ready(function() {
   $("#questions").submit(function(event) {
@@ -16,17 +20,19 @@ $(document).ready(function() {
     let content = parseInt($("input[name='content']:checked").val());
     let total = totalValue (operation, cost, think, job, content);
 
-  if (total >=16) {
-    $("#swift").show();
-  } else if (total >=13) {
-    $("#ruby").show();
-  } else if (total >=10) {
-    $("#python").show();
-  } else if (total >=7) {
-    $("#csharp").show();
-  } else if (total >=5) { 
-    $("#javascript").show();
-  }
+    if ($("input[type='radio']") !== (":checked")) {
+      alert("Did you miss a question?");
+    } else if (total >=16) {
+      $("#swift").show();
+    } else if (total >=13) {
+      $("#ruby").show();
+    } else if (total >=10) {
+      $("#python").show();
+    } else if (total >=7) {
+      $("#csharp").show();
+    } else if (total >=5) { 
+      $("#javascript").show();
+    }
 
     $(".result").show();
     $(".name").text(user);
